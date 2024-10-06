9f8aebc0792091bde5f53189b31cd4824c0bc187
Gauri kute <144440470+gaurikute270@users.noreply.github.com>
2024-10-06 06:04:21 -0700

    Update

---
+++ Collection3.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+class Student{
+int rollno;
+String name;
+int age;
+Student(int rollno,String name,int age){
+this.rollno=rollno;
+this.name=name;
+this.age=age;
+}
+}//userdefined
+public class Collection3 {
+public static void main(String args[]) {
+Student s1=new Student(101,"sonoo",23);
+Student s2=new Student(102,"sima",24);
+Student s3=new Student(103,"sonal",25);
+java.util.ArrayList<Student> al = new java.util.ArrayList<>();
+al.add(s1);
+al.add(s2);
+al.add(s3);
+Iterator<Student> itr = al.iterator();
+while(itr.hasNext()) {
+Student st=itr.next();
+System.out.println(st.rollno + " " + st.name + " " + st.age);
+}
+}
+}

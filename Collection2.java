06d4578d8d5a65f2971b5b90f1f63ea1936e9be4
Gauri kute <144440470+gaurikute270@users.noreply.github.com>
2024-10-06 06:03:09 -0700

    Update

---
+++ Collection2.java
@@ -0,0 +1,10 @@
+public class Collection2 {
+public static void main(String args[]) {
+java.util.ArrayList<String> al = new java.util.ArrayList<>();
+al.add("ravi");
+al.add("ravi");
+al.add("ravi");
+for(String obj:al)
+System.out.println(obj);
+}
+}
